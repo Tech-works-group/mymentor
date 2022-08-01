c6504cc20727e3d1c076fcafd1ff5114bf3b8bac
balqeessabir <balqeessaber@gmail.com>
2022-08-01 15:14:24 +0200

    more fontawesome icons

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 import '@nextcss/reset';
 import "@fortawesome/fontawesome-svg-core/styles.css"; 
 import { config } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 import '../styles/globals.css'
 import Layout from '../common/Layout';
 
